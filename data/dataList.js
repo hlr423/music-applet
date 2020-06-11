b09e7cdf93ef2a0d2602a40a81f5ead1b8ba4b13
黄利蓉 <huanglirong_fh@12530.com>
2020-06-11 11:40:09 +0800

    commit

---
+++ data/dataList.js
@@ -0,0 +1,57 @@
+
+ let  singerData=[
+    {
+      id: 1,
+      title: '破茧',
+      singer: '张韶涵',
+      src: 'http://ws.stream.qqmusic.qq.com/C400000BWALf24B9TT.m4a?guid=2350206416&vkey=B32D0A16BAB6032619763AEB57367314375416FF85D548F4A86D8CE8F9CFFA57F435ABBFA580037C1B37B994B28410E3A54E14A51A272F7D&uin=0&fromtag=66',
+      coverImgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1591855245343&di=a51969f36823a3c4f9cdaea70f0ac288&imgtype=0&src=http%3A%2F%2Fimg.kanjianlishi.com%2Fstar%2Fa8e7da7955f7f174fed3bd2cca4fd9b8.jpg'
+    },
+    {
+      id: 2,
+      title: '旧梦一场',
+      singer: '阿悠悠',
+      src: 'http://ws.stream.qqmusic.qq.com/C4000049iR603EJHAY.m4a?guid=2350206416&vkey=67505A6E4DD373EDA42368B3489B5233014742C65B806248756A7157B254ADD2715BBB58779A7AFD4CFB5BED46FF2E9BBC436634ADDD1BF6&uin=0&fromtag=66',
+      coverImgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1591855454076&di=e35c8dd8114bc9c5ef71aa01d3ee8c84&imgtype=0&src=http%3A%2F%2Fwww.tuzizs.com%2Fupfile%2F201911%2F2019112632853873.jpg'
+    },
+    {
+      id: 3,
+      title: '万有引力',
+      singer: '汪苏泷',
+      src: 'http://ws.stream.qqmusic.qq.com/C400001kayD73x4oEu.m4a?guid=2350206416&vkey=BE4E618AE3CF288E10722543E83BFD5F651B26BF39C5E593370AC262EE789E5D67B10C1FF1BB84980A53887493A6264D2976F415E02FA5DA&uin=0&fromtag=66',
+      coverImgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1591855542015&di=c107060e37a35172d2f8a0cf32c2319b&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201807%2F11%2F20180711235919_vrqwl.jpeg'
+    },
+    {
+      id: 4,
+      title: '素颜',
+      singer: '许嵩',
+      src: 'http://ws.stream.qqmusic.qq.com/C400001sE4o50UXy4q.m4a?guid=2350206416&vkey=E63C750EDE910CD5CCEE1779A4FDA5E719B1C0822F45286AA9C0C2303E4BB0A3ED597A61B618BD26FA5A6A8EAB85277B163ADE972CC8F429&uin=0&fromtag=66',
+      coverImgUrl: 'https://ss1.baidu.com/9vo3dSag_xI4khGko9WTAnF6hhy/imgad/pic/item/3812b31bb051f8192aca6141d2b44aed2f73e7ca.jpg'
+    },
+    {
+      id: 5,
+      title: 'Sour Candy',
+      singer: 'Lady Gaga',
+      src: 'http://ws.stream.qqmusic.qq.com/C400001Pt5cg4FdhB0.m4a?guid=2350206416&vkey=DA01B017DF3256EBEC1653184604E5A0AFAEEE3CFB7542E80C473C46F561DE82A577F6DA1C604B87A6164DEE0A8E7EAA8DD2AF2F20D6AA5E&uin=0&fromtag=66',
+      coverImgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1591855875616&di=20f9bf2802fca2b5959dc0ad19190eb1&imgtype=0&src=http%3A%2F%2Fimg.zybus.com%2Fuploads%2Fallimg%2F131221%2F1-131221152433-50.jpg'
+    },
+    {
+      id: 3,
+      title: '后继者',
+      singer: '任然',
+      src: 'http://ws.stream.qqmusic.qq.com/C400003MdBGh2TUjaq.m4a?guid=2350206416&vkey=73DACF6F9F73B1B9E58035A2DE1FA1C745A30F31CFD8532A5FC068CBE17DB109F9AC643D882700736B24E1BA4CCD6F82EBFE4EF644775B3D&uin=0&fromtag=66',
+      coverImgUrl: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4043754626,4212144384&fm=26&gp=0.jpg'
+    },
+  ]
+  let playValue={
+    currentTime: '00:00',
+    duration: '00:00',
+    percent: 0,
+    title: '莲花',
+    singer: '周延',
+    coverImgUrl: '/pages/images/zly.jpeg'
+  }
+module.exports={
+  singerData,
+  playValue
+}
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,40 +1,13 @@
+let app =getApp()
+let {singerData,playValue} =require('../../data/dataList')
 Page({
   data: {
     current: 0,
     tab: 0,
-    playList: [
-      {
-        id: 2,
-        title: '一点点',
-        singer: '陈意涵',
-        src: 'http://isure.stream.qqmusic.qq.com/C400004IQJ3g4SlxYN.m4a?guid=2350206416&vkey=BBF34FD1FF1284E1292179D803EB982FD1487FC3A56AEE341BDCA9E4961D6B1B91595D5362AA328DAFF467ECBC1F4EC984716DDC74E9B4EE&uin=3377&fromtag=66',
-        coverImgUrl: '/pages/images/zly.jpeg'
-      },
-      {
-        id: 1,
-        title: '莲花',
-        singer: '周延',
-        src: 'http://ws.stream.qqmusic.qq.com/C400003PM62v4FyOqS.m4a?guid=2350206416&vkey=48A46C79CB55C30D65D65E3C5BE3A640C6EBE606B1346C6202E0BB1271BDC72AC6A14C4041932A7077EA0D7A04CD05CDA727A5FD513EDFF0&uin=3377&fromtag=66',
-        coverImgUrl: '/pages/images/zly.jpeg'
-      },
-      {
-        id: 3,
-        title: '命中注定',
-        singer: '丁当',
-        src: 'http://39.130.175.145/amobile.music.tc.qq.com/C400002mMW4D0I8fI8.m4a?guid=2350206416&vkey=7BE9481D114E436756D002D996AD16624040FA907F5BF902878F7C3AB83187A3D81B7B29185D127152ECCDC79D1A7690603E006D4C4B002A&uin=3377&fromtag=66',
-        coverImgUrl: '/pages/images/zly.jpeg'
-      },
-    ],
+    playList: singerData,
     state: 'paused',
     playIndex: 0,
-    play: {
-      currentTime: '00:00',
-      duration: '00:00',
-      percent: 0,
-      title: '莲花',
-      singer: '周延',
-      coverImgUrl: '/pages/images/zly.jpeg'
-    },
+    play: playValue,
   },
   audioCtx: null,
   changeBar(e) {
@@ -56,11 +29,10 @@ Page({
     })
   },
   onReady: function () {
-    console.log(this.audioCtx)
     this.audioCtx = wx.createInnerAudioContext();
     let that =this;
     this.audioCtx.onError(function(error){
-      console.log('播放失败'+this.audioCtx.src)
+      console.log('播放失败',error)
     })
     // 播放完成自动换下一曲
     this.audioCtx.onEnded(function(){
@@ -133,4 +105,9 @@ Page({
      this.play()
    }
   },
+  // 播放列表换曲功能
+  change(e){
+    console.log(e)
+    this.setMusic(e.currentTarget.dataset.index)
+  }
 })
